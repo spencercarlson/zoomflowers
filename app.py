550a5c945452b0a2f1ee2dd7566cd79def07cd60
Spencer Carlson <spencercarlson2017@u.northwestern.edu>
2021-05-07 13:06:33 -0700

    Try?

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def index():
-    flowers = os.listdir('static/flowers')
+    flowers = os.listdir('/static/flowers')
     flowers.remove('.DS_Store')
     return render_template('index.html',flowers=flowers)
