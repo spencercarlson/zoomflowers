57f45e3fc64999323953df3ba76e12d8e6e47992
Spencer Carlson <spencercarlson2017@u.northwestern.edu>
2021-05-07 13:57:46 -0700

    Annotation

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-# app.url_map.strict_slashes = False
+app.url_map.strict_slashes = False
 
 @app.route('/')
 def index():
     flowers = os.listdir('static/flowers')
     print(flowers)
     # flowers.remove('.DS_Store')
+    # ^ this line is only needed on the Mac OS - if you include when you deploy to Netlify it will break things :) 
     print(flowers)
     return render_template('index.html',flowers=flowers)
