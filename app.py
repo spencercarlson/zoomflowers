8a78e803a5b027b0a9e2dccb12ebde6be76c3390
Spencer Carlson <spencercarlson2017@u.northwestern.edu>
2021-05-07 14:04:42 -0700

    Sorted flowers

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ app.url_map.strict_slashes = False
 @app.route('/')
 def index():
     flowers = os.listdir('static/flowers')
-    print(flowers)
+    flowers.sort()
     # flowers.remove('.DS_Store')
     # ^ this line is only needed on the Mac OS - if you include when you deploy to Netlify it will break things :) 
-    print(flowers)
     return render_template('index.html',flowers=flowers)
