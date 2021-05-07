6789145391b0d2391dcc859a595551ad7f67f798
Spencer Carlson <spencercarlson2017@u.northwestern.edu>
2021-05-07 13:52:06 -0700

    New test

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.url_map.strict_slashes = False
+# app.url_map.strict_slashes = False
 
 @app.route('/')
 def index():
     flowers = os.listdir('static/flowers')
     print(flowers)
     flowers.remove('.DS_Store')
+    print(flowers)
     return render_template('index.html',flowers=flowers)
