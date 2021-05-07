f10fb43e74ab33c3facd8909c4616795f24a542d
Spencer Carlson <spencercarlson2017@u.northwestern.edu>
2021-05-07 11:59:04 -0700

    app

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def index():
+    flowers = os.listdir('static/flowers')
+    flowers.remove('.DS_Store')
+    return render_template('index.html',flowers=flowers)
